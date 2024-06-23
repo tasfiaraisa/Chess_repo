1e454c2e268d2b13954e28c44808ca57829feb61
Megh778 <64145406+Megh778@users.noreply.github.com>
2024-06-22 19:19:12 -0600

    Update pieces.py
    Bishop function added

--- pieces.py
+++ pieces.py
@@ -58,7 +58,7 @@ def pawnMoveset(row, col, color, board, firstMove):
 
 # #Queen Moves
 # #Code here
-# def queenMoveset(position, color, board, firstMove)
+# def queenMoveset(position, color, board)
 
 
 
@@ -69,7 +69,7 @@ def pawnMoveset(row, col, color, board, firstMove):
 
 # #Rook Moves
 # #Code here
-# def rookMoveset(position, color, board, firstMove)
+# def rookMoveset(position, color, board)
     
 
 
@@ -79,21 +79,41 @@ def pawnMoveset(row, col, color, board, firstMove):
 
 ##########################################################################################################################################
 
-# #Bishop Moves
-# #Code here
-# def bishopMoveset(position, color, board, firstMove)
+#Bishop Moves
+#Code here
+def bishopMoveset(row, col, color, board):
+    moves = []
+    directions = [(-1, -1), (-1, 1), (1, -1), (1, 1)]  #Diagonal movements: top-left, top-right, bottom-left, bottom-right
+
+    #Check each of the four possible diagonal directions
+    for d in directions:
+        for i in range(1, 8):  #bishop can move up to 7 squares in each direction
+            newRow, newCol = row + d[0] * i, col + d[1] * i
+            if 0 <= newRow < 8 and 0 <= newCol < 8:  #Ensure that the position is still on the board
+                if board[newRow][newCol] == 0:  #The square is empty
+                    moves.append((newRow, newCol))
+                elif whatColor(board[newRow][newCol]) != color:  #The square contains an enemy piece
+                    moves.append((newRow, newCol))
+                    break  #Stop extending in this direction after a capture
+                else:
+                    break  #Stop if there is a piece of the same color
+            else:
+                break  #Stop if out of board bounds
+
+    return moves
 
 
 
 
 
-# #
+
+#
 
 ##########################################################################################################################################
 
 # #Knight Moves
 # #Code here
-# def knightMoveset(position, color, board, firstMove)
+# def knightMoveset(position, color, board)
 
 
 
